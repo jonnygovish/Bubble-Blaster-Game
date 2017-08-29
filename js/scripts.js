18e17ff39bc197e8394e9a2a978a5b7abdf063ae
John <jonnygovish@gmail.com>
2017-08-29 15:46:21 +0300

    OVERLAY for the about section

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,13 @@
-// $(document).ready(function () {
-//   $(".about").on('click', function (event) {
-//     event.preventDefault();
-//     $('.overlay').addClass('open');
-//     console.log("open");
-//   });
-//   $('.closeNav').on('click', function (event) {
-//     event.preventDefault();
-//     $('.overlay').removeClass('open');
-//   });
-//   console.log();
-// });
+ $(document).ready(function () {
+   $(".about").on('click', function (event) {
+     event.preventDefault();
+     $(".overlay").addClass('open');
+     console.log("open");
+   });
+   $(".closeNav").on('click', function (event) {
+     event.preventDefault();
+     $(".overlay").removeClass('open');
+   });
+     
+   console.log();
+ });
