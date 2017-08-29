d3ebdef991830609b7287c906f2fb126bc7c28e2
marynganga <marynganga.mw@gmail.com>
2017-08-29 19:55:13 +0300

    Input content on scripts.js.

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+////Front End
+//$(document).ready(function(){
+//$('#start').submit(function (event) {
+//    var min = 0;
+//    var sec = 30;
+//    var myVar = setInterval(function () {
+//        countDownTimer()
+//    }, 500);
+//
+//    function countDownTimer() {
+//        $(".timer").text(min + " : " + sec);
+//        sec--;
+//        if (sec === -1) {
+//            clearInterval(myVar);
+//        };
+//    };
+//    countDownTimer();
+//    console.log(countDownTimer();)
+//});
+//});
