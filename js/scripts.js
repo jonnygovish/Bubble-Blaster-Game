b788ae203955df010af11145f2d1cbf5bb9ed361
Ndolo S <shadrack.winters@outlook.com>
2017-08-29 11:47:03 +0300

    Initializing the landing page. (HTML) Setting up the navigation bar and header. Making a carousel for an animated image display. Creating a button for play and a name input section. (CSS) Margins for the button. (js) Commented out.

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+// $(document).ready(function () {
+//   $(".about").on('click', function (event) {
+//     event.preventDefault();
+//     $('.overlay').addClass('open');
+//     console.log("open");
+//   });
+//   $('.closeNav').on('click', function (event) {
+//     event.preventDefault();
+//     $('.overlay').removeClass('open');
+//   });
+//   console.log();
+// });
