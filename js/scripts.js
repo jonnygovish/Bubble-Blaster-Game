52304db9ebec2af8158f7544cf8375715ce4ddca
Carol Wanjohi <wanjohi.carol54@gmail.com>
2017-08-30 12:40:23 +0300

    Selecting background colors 1. Create .change() to prompt a change in the background color when a user selects an option

--- js/scripts.js
+++ js/scripts.js
@@ -9,8 +9,8 @@ $(document).ready(function () {
     // Test
     // console.log("open");
   });
-    
-    // Close the about overlay
+
+  // Close the about overlay
   $('.closeNav').on('click', function (event) {
     event.preventDefault();
     $('.overlay').removeClass('open');
@@ -22,8 +22,34 @@ $(document).ready(function () {
     window.location.assign("play.html");
       
   });
+
+  // Selecting background colors
+  $('.bg-colors').change(function () {
+
+    $('body').removeClass();
+
+    if ($(this).val() === 'blue') {
+      $('body').addClass('blue');
+    } else if ($(this).val() === 'violet') {
+      $('body').addClass('violet');
+    } else if ($(this).val() === 'aqua') {
+      $('body').addClass('aqua');
+    } else if ($(this).val() === 'lilac') {
+      $('body').addClass('lilac');
+    } else {
+      $('body').addClass('default');
+    }
+
+  });
+  // var chosenColor = $('select.bg-colors option:checked').val();
+  // if (chosenColor === "Blue") {
+  //   $('body').addClass('blue');
+  //   console.log("I am groot");
+  // } else {
+  //   return false;
+  // }
   
-    // Click the start button for the timer to start
+  // Click the start button for the timer to start
   $('#start').click(function (event) { 
     event.preventDefault();
     
