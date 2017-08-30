92fa548dd7fa642cfa6e459e8f44622fb7a5d2cb
marynganga <marynganga.mw@gmail.com>
2017-08-30 19:25:10 +0300

    Interchangeable background-colors 1.Create div class 'overlay' in play.html and index.html to change the overlay color of the body's background image. 2.Update the .change() function in scripts.js to work alter colours in the new division created. 3.Update the colors in the inter-changeable classes to be of rgba format.

--- js/scripts.js
+++ js/scripts.js
@@ -2,70 +2,74 @@
 
 // Front-end
 $(document).ready(function () {
-  // Make about section clickable and open about overlay
-  $(".about").on('click', function (event) {
-    event.preventDefault();
-    $('.overlay').addClass('open');
-    // Test
-    // console.log("open");
-  });
+    // Make about section clickable and open about overlay
+    $(".about").on('click', function (event) {
+        event.preventDefault();
+        $('.overlay').addClass('open');
+        // Test
+        // console.log("open");
+    });
 
-  // Close the about overlay
-  $('.closeNav').on('click', function (event) {
-    event.preventDefault();
-    $('.overlay').removeClass('open');
-  });
-    
-  // Navigate to play console
-  $('form.player').submit( function (event) {
-    event.preventDefault();
-    window.location.assign("play.html");
-      
-  });
+    // Close the about overlay
+    $('.closeNav').on('click', function (event) {
+        event.preventDefault();
+        $('.overlay').removeClass('open');
+    });
 
-  // Selecting background colors
-  $('.bg-colors').change(function () {
+    // Navigate to play console
+    $('form.player').submit(function (event) {
+        event.preventDefault();
+        window.location.assign("play.html");
 
-    $('body').removeClass();
+    });
 
-    if ($(this).val() === 'blue') {
-      $('body').addClass('blue');
-    } else if ($(this).val() === 'violet') {
-      $('body').addClass('violet');
-    } else if ($(this).val() === 'aqua') {
-      $('body').addClass('aqua');
-    } else if ($(this).val() === 'lilac') {
-      $('body').addClass('lilac');
-    } else {
-      $('body').addClass('default');
-    }
+    // Selecting background colors
+    $('.bg-colors').change(function () {
+        var colorsChosen = ['blue','violet','aqua','lilac','default'];
+        colorsChosen.forEach(function(colorChosen){
+            $('.colorOverlay').removeClass(colorChosen);
+        });
+//        $('.colorOverlay').removeClass();
 
-  });
-  // var chosenColor = $('select.bg-colors option:checked').val();
-  // if (chosenColor === "Blue") {
-  //   $('body').addClass('blue');
-  //   console.log("I am groot");
-  // } else {
-  //   return false;
-  // }
-  
-  // Click the start button for the timer to start
-  $('#start').click(function (event) { 
-    event.preventDefault();
-    
-    var min = 0;
-    var sec = 30;
+        if ($(this).val() === 'blue') {
+            $('.colorOverlay').addClass('blue');
+        } else if ($(this).val() === 'violet') {
+            $('.colorOverlay').addClass('violet');
+        } else if ($(this).val() === 'aqua') {
+            $('.colorOverlay').addClass('aqua');
+        } else if ($(this).val() === 'lilac') {
+            $('.colorOverlay').addClass('lilac');
+        } else {
+            $('.colorOverlay').addClass('default');
+        }
 
-    var myVar = setInterval( function() { countDownTimer() } ,1000 );
+    });
+    // var chosenColor = $('select.bg-colors option:checked').val();
+    // if (chosenColor === "Blue") {
+    //   $('body').addClass('blue');
+    //   console.log("I am groot");
+    // } else {
+    //   return false;
+    // }
 
-    function countDownTimer() {
+    // Click the start button for the timer to start
+    $('#start').click(function (event) {
+        event.preventDefault();
 
-    var displayCountdown = $('.timer').text( min +" : " + sec );
-    sec--;
-    if(sec === -1) {
-       clearInterval(myVar);
-    };
-    };
-  });
+        var min = 0;
+        var sec = 30;
+
+        var myVar = setInterval(function () {
+            countDownTimer()
+        }, 1000);
+
+        function countDownTimer() {
+
+            var displayCountdown = $('.timer').text(min + " : " + sec);
+            sec--;
+            if (sec === -1) {
+                clearInterval(myVar);
+            };
+        };
+    });
 });
-
